3ed754c92a05b6fd0b583abc18b1fc9044069484
Justin <jerwei.yeoh@exwzd.com>
2022-11-13 15:33:38 +0900

    semver: add bye function

--- semver/mod1/user.go
+++ semver/mod1/user.go
@@ -12,6 +12,10 @@ func (u *User) SayHi() string {
 	return fmt.Sprintf("Hi there, I'm %s.", u.Name)
 }
 
+func (u *User) SayBye() string {
+	return "Bye!"
+}
+
 func NewUser(name string) User {
 	return User{
 		Name: name,
--- semver/mod1/user_test.go
+++ semver/mod1/user_test.go
@@ -2,12 +2,26 @@ package mod1
 
 import "testing"
 
-func Test_User(t *testing.T) {
+func Test_User_SayHi(t *testing.T) {
 	name := "James"
 	expected := "Hi there, I'm James."
-	u := NewUser(name)
 
-	if u.SayHi() != expected {
-		t.Errorf("execpted '%s' but got '%s'", expected, u.SayHi())
+	u := NewUser(name)
+	actual := u.SayHi()
+
+	if actual != expected {
+		t.Errorf("execpted '%s' but got '%s'", expected, actual)
+	}
+}
+
+func Test_User_SayBye(t *testing.T) {
+	name := "James"
+	expected := "Bye!"
+
+	u := NewUser(name)
+	actual := u.SayBye()
+
+	if actual != expected {
+		t.Errorf("execpted '%s' but got '%s'", expected, actual)
 	}
 }
